a1f60b2bf26812e9e8ae9e762e3395c5563d06f6
Apurbo73 <apurbodebnath895@gmail.com>
2023-05-31 23:13:58 +0600

    second commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import useReducers from "./component/useReducers";
+
+function App() {
+  return (
+    <div>
+      <useReducers />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/src/component/useReducers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const useReducers = () => {
+    return (
+        <div>
+            hi
+        </div>
+    );
+};
+
+export default useReducers;
